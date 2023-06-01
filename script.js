2ff7d40079e244bd1299a52fb6fb931ba548b614
shrutijain2296 <128174123+shrutijain2296@users.noreply.github.com>
2023-06-01 15:29:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 //your JS code here. If required.
-const level = document.getElementById("level");
+const level = document.getElementById("level").value;
 alert("The level of the element is:" level);
