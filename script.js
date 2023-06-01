38d26a8700f7747e09ead981fa3156af726db997
shrutijain2296 <128174123+shrutijain2296@users.noreply.github.com>
2023-06-01 15:57:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 //your JS code here. If required.
-const level = document.querySelectorAll("ul li")
-const domLevel = -1;
+let level = document.querySelectorAll("ul li")
+let domLevel = -1;
 
-for(let i = 0; i < level.length; i++){
-	if(level[i.id === "level"){
-		domLevel = i+1;
-		break;
-	}
-	
+for (let i = 0; i < level.length; i++) {
+  if (level[i].id === "level") {
+    domLevel = i + 1;
+    break;
+  }
 }
-alert("The level of the element is:" + domLevel);
+alert("The level of the element is: " + domLevel);
 
 
 
